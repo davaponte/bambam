7edab258440fb77f6dbe2158474da082cebbbd40
David Aponte <david.aponte@outlook.com>
2016-12-02 10:56:28 -0500

    Agregado de info de desarrollador en cabecera de programa

--- bambam.py
+++ bambam.py
@@ -3,6 +3,7 @@
 #    2007-2008 Don Brown,
 #    2010 Spike Burch <spikeb@gmail.com>,
 #    2015-2016 Vasya Novikov
+#    2016 David Aponte <david.aponte@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
